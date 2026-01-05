3e99dfe9d6ab1ccb24f5621e6c0a96ec649fb3cc
Jan T. Sott <git@idleberg.com>
2026-01-05 19:47:50 +0100

    chore: format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,21 @@
-/* eslint-disable */
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es6: true,
-    node: true
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:coffee/eslint-recommended',
-    'plugin:json/recommended'
-  ],
-  globals: {
-    atom: "readonly"
-  },
-  parser: 'eslint-plugin-coffee',
-  plugins: ['coffee'],
-  rules: {
-    'coffee/no-useless-escape': 'off'
-  }
+	root: true,
+	env: {
+		browser: true,
+		es6: true,
+		node: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:coffee/eslint-recommended',
+		'plugin:json/recommended'
+	],
+	globals: {
+		atom: "readonly"
+	},
+	parser: 'eslint-plugin-coffee',
+	plugins: ['coffee'],
+	rules: {
+		'coffee/no-useless-escape': 'off'
+	}
 };
--- src/nsl.ts
+++ src/nsl.ts
@@ -3,69 +3,69 @@ import { notifyOnSucess } from "./util.js";
 import meta from "../package.json";
 
 export function transpile(consolePanel: any): void {
-  const editor = atom.workspace.getActiveTextEditor();
+	const editor = atom.workspace.getActiveTextEditor();
 
-  if (editor == null) {
-    atom.notifications.addWarning(`**${meta.name}**: No active editor`, { dismissable: false });
-    return;
-  }
+	if (editor == null) {
+		atom.notifications.addWarning(`**${meta.name}**: No active editor`, { dismissable: false });
+		return;
+	}
 
-  const script = editor.getPath();
-  const scope = editor.getGrammar().scopeName;
+	const script = editor.getPath();
+	const scope = editor.getGrammar().scopeName;
 
-  if ((script != null) && scope.startsWith("source.nsl")) {
-    editor.save().then(() => {
-      const nslJar = atom.config.get("language-nsl.pathToJar") as string;
-      const defaultArguments = ["-jar", `${nslJar}`];
-      const customArguments = [...atom.config.get("language-nsl.customArguments") as string[]];
-      customArguments.push(script);
-      const args = defaultArguments.concat(customArguments);
+	if ((script != null) && scope.startsWith("source.nsl")) {
+		editor.save().then(() => {
+			const nslJar = atom.config.get("language-nsl.pathToJar") as string;
+			const defaultArguments = ["-jar", `${nslJar}`];
+			const customArguments = [...atom.config.get("language-nsl.customArguments") as string[]];
+			customArguments.push(script);
+			const args = defaultArguments.concat(customArguments);
 
-      try {
-        consolePanel.clear();
-      } catch (error) {
-        if (atom.config.get("language-nsl.clearConsole")) {
-          console.clear();
-        }
-      }
+			try {
+				consolePanel.clear();
+			} catch (error) {
+				if (atom.config.get("language-nsl.clearConsole")) {
+					console.clear();
+				}
+			}
 
-      // Let's go
-      const nslCmd = spawn("java", args);
-      let hasError = false;
+			// Let's go
+			const nslCmd = spawn("java", args);
+			let hasError = false;
 
-      nslCmd.stdout.on("data", (data: Buffer) => {
-        try {
-          if (atom.config.get("language-nsl.alwaysShowOutput")) {
-            consolePanel.log(data.toString());
-          }
-        } catch (error) {
-          console.log(data.toString());
-        }
-      });
+			nslCmd.stdout.on("data", (data: Buffer) => {
+				try {
+					if (atom.config.get("language-nsl.alwaysShowOutput")) {
+						consolePanel.log(data.toString());
+					}
+				} catch (error) {
+					console.log(data.toString());
+				}
+			});
 
-      nslCmd.stderr.on("data", (data: Buffer) => {
-        hasError = true;
-        try {
-          consolePanel.error(data.toString());
-        } catch (error) {
-          console.error(data.toString());
-        }
-      });
+			nslCmd.stderr.on("data", (data: Buffer) => {
+				hasError = true;
+				try {
+					consolePanel.error(data.toString());
+				} catch (error) {
+					console.error(data.toString());
+				}
+			});
 
-      nslCmd.on("close", (errorCode: number | null) => {
-        if ((errorCode === 0) && (hasError === false)) {
-          if (atom.config.get("language-nsl.showBuildNotifications")) {
-            notifyOnSucess();
-          }
-        } else {
-          if (atom.config.get("language-nsl.showBuildNotifications")) {
-            atom.notifications.addError("Transpile failed", { dismissable: false });
-          }
-        }
-      });
-    });
-  } else {
-    // Something went wrong
-    atom.beep();
-  }
+			nslCmd.on("close", (errorCode: number | null) => {
+				if ((errorCode === 0) && (hasError === false)) {
+					if (atom.config.get("language-nsl.showBuildNotifications")) {
+						notifyOnSucess();
+					}
+				} else {
+					if (atom.config.get("language-nsl.showBuildNotifications")) {
+						atom.notifications.addError("Transpile failed", { dismissable: false });
+					}
+				}
+			});
+		});
+	} else {
+		// Something went wrong
+		atom.beep();
+	}
 }
